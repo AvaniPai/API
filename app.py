32dca9311f778d5c615fecbe2f65caef81bc7241
AvaniPai <AvaniPai@wirelessprvnat-172-17-4-49.near.illinois.edu>
2015-04-21 19:06:20 -0500

    flask app.py

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify, request, render_template, session
+from flask import Flask
+from flask import Flask, jsonify, request, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+from models import *
+
+@app.route('/')
+def hello():
+	return "Welcome to Opshun!"
+
+@app.route('/<username>')
+def specialized(username):
+	return "Hello {}!".format(username)
+
+@app.route('/login', methods=['GET','POST'])
+def signingup():
+	errors = []
+	results= {}
+	if request.method == "POST":
+		email = request.form['email']
+		password = request.form['newpass']
+		foundAt = email.index('@')
+		user = email[:foundAt]
+		try:
+			newUser = User(user, email, password)
+			db.session.add(newUser)
+			db.session.commit()
+		except:
+			errors.append("unable to add item to databse.")
+	return render_template('login.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
